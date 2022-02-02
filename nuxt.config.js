d73d4b6d16809cfb73aaba66f71d97f0f22688fd
Evgeniy Shek <shek.yujin@gmail.com>
2022-02-02 17:33:58 +0600

    chore(nuxt): register @nuxtjs/router

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    // https://github.com/nuxt-community/router-module
+    '@nuxtjs/router',
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
